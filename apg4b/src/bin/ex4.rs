145e6e6f8a2c6f0d802242f337d438430e6dce84
yoshiharu2580 <yoshiharu2580@gmail.com>
2021-04-11 15:50:50 +0900

    Add apg4b ex4

--- apg4b/src/bin/ex4.rs
+++ apg4b/src/bin/ex4.rs
@@ -1,3 +1,8 @@
 fn main() {
-    todo!();
+    let seconds: u32 = 365 * 24 * 60 * 60;
+
+    println!("{}", seconds);
+    println!("{}", seconds * 2);
+    println!("{}", seconds * 5);
+    println!("{}", seconds * 10);
 }
