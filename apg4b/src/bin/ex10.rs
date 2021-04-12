39e7b286954a414caa67209a0519651382d3f08c
yoshiharu2580 <yoshiharu2580@gmail.com>
2021-04-13 00:52:19 +0900

    Add apg4b ex10

--- apg4b/src/bin/ex10.rs
+++ apg4b/src/bin/ex10.rs
@@ -1,3 +1,19 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        A: u32,
+        B: u32,
+    }
+
+    let mut a = String::new();
+    let mut b = String::new();
+
+    for _ in 0..A {
+        a += "]";
+    }
+    for _ in 0..B {
+        b += "]";
+    }
+
+    println!("A:{}", a);
+    println!("B:{}", b);
 }
