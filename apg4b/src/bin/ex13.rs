b6f480a2c8a1ff0932ea778e38f074b5e8dfd025
Your Name <you@example.com>
2021-04-18 02:20:34 +0900

    apg4b/ex13

--- apg4b/src/bin/ex13.rs
+++ apg4b/src/bin/ex13.rs
@@ -1,3 +1,17 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        a: [i32; n],
+    }
+
+    let mut sum = 0;
+    for i in &a {
+        sum += i;
+    }
+
+    let average = sum / (n as i32);
+
+    for i in &a {
+        println!("{}", (average - i).abs())
+    }
 }
