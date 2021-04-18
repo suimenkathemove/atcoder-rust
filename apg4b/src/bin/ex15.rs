f17c12a6d9b44f0ac33297ef4fa6081cc0b2f3ef
Your Name <you@example.com>
2021-04-18 16:14:21 +0900

    apg4b/ex15

--- apg4b/src/bin/ex15.rs
+++ apg4b/src/bin/ex15.rs
@@ -1,3 +1,18 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        a: [i32; n],
+        b: [i32; n],
+        c: [i32; n],
+    }
+
+    fn sum(vec: Vec<i32>) -> i32 {
+        let mut sum = 0;
+        for num in &vec {
+            sum += num;
+        }
+        sum
+    }
+
+    println!("{}", sum(a) * sum(b) * sum(c))
 }
