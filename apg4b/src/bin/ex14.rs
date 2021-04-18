6b4cb0a68d6a4a67affe8e7668bf8da8efd3f5e0
Your Name <you@example.com>
2021-04-18 16:08:21 +0900

    apg4b/ex14

--- apg4b/src/bin/ex14.rs
+++ apg4b/src/bin/ex14.rs
@@ -1,3 +1,11 @@
+use std::cmp::{max, min};
+
 fn main() {
-    todo!();
+    proconio::input! {
+        a: i32,
+        b: i32,
+        c: i32,
+    }
+
+    println!("{}", max(max(a, b), c) - min(min(a, b), c));
 }
