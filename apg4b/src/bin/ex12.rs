f0a8d9a35545ab27113033de98d868ee5a14bf29
Your Name <you@example.com>
2021-04-18 11:05:44 +0900

    apg4b/ex12

--- apg4b/src/bin/ex12.rs
+++ apg4b/src/bin/ex12.rs
@@ -1,3 +1,17 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        s: String,
+    }
+
+    let mut ans = 1;
+
+    for c in s.chars() {
+        match c {
+            '+' => ans += 1,
+            '-' => ans -= 1,
+            _ => (),
+        }
+    }
+
+    println!("{}", ans);
 }
