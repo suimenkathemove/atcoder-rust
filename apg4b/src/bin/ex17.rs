0283d4e44232f3f69efccf3548955dcdeadb0318
Your Name <you@example.com>
2021-04-18 17:36:29 +0900

    ex17

--- apg4b/src/bin/ex17.rs
+++ apg4b/src/bin/ex17.rs
@@ -1,3 +1,20 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        s: i32,
+        a: [i32; n],
+        p: [i32; n],
+    }
+
+    let mut ans = 0;
+
+    for i in &a {
+        for j in &p {
+            if i + j == s {
+                ans += 1;
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
