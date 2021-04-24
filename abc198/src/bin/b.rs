6290780753408db5fc8ebb5ca52645d1be603a58
Your Name <you@example.com>
2021-04-24 09:24:10 +0900

    ABC198/b

--- abc198/src/bin/b.rs
+++ abc198/src/bin/b.rs
@@ -1,3 +1,17 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: i32,
+    }
+
+    let str_n = n.to_string();
+    let trimmed_str_n = str_n.trim_end_matches('0');
+
+    println!(
+        "{}",
+        if trimmed_str_n == trimmed_str_n.chars().rev().collect::<String>() {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
