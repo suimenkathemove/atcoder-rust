157d1940560b114bfbb4f2ce0ede404abd82772a
Your Name <you@example.com>
2021-07-21 08:23:01 +0900

    arc046/a brute_force

--- arc046/src/bin/a.rs
+++ arc046/src/bin/a.rs
@@ -1,3 +1,25 @@
-fn main() {
-    todo!();
+fn main() {}
+
+fn brute_force() {
+    proconio::input! {
+        n: usize,
+    }
+
+    let mut count = 0;
+
+    let mut ans = 0;
+
+    for i in 1.. {
+        let mut v: Vec<char> = i.to_string().chars().collect();
+        v.dedup();
+        if v.len() == 1 {
+            count += 1;
+            if count >= n {
+                ans = i;
+                break;
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
