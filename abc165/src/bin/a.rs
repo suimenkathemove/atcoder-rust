2b2a5cea5bbd1d9f749d587d5a845bf7dcc4e7c7
Your Name <you@example.com>
2021-07-21 20:47:24 +0900

    abc165/a

--- abc165/src/bin/a.rs
+++ abc165/src/bin/a.rs
@@ -1,3 +1,58 @@
-fn main() {
-    todo!();
+fn main() {}
+
+fn main1() {
+    proconio::input! {
+        k: usize,
+        a: usize,
+        b: usize,
+    }
+
+    let mut is_ok = false;
+
+    for i in a..=b {
+        if i % k == 0 {
+            is_ok = true;
+        }
+    }
+
+    println!("{}", if is_ok { "OK" } else { "NG" });
+}
+
+fn main2() {
+    proconio::input! {
+        k: usize,
+        a: usize,
+        b: usize,
+    }
+
+    let mut n = k;
+
+    let mut is_ok = false;
+
+    loop {
+        if a <= n && n <= b {
+            is_ok = true;
+            break;
+        }
+
+        if b < n {
+            break;
+        }
+
+        n += k;
+    }
+
+    println!("{}", if is_ok { "OK" } else { "NG" });
+}
+
+fn main3() {
+    proconio::input! {
+        k: usize,
+        a: usize,
+        b: usize,
+    }
+
+    let is_ok = a / k < b / k || a % k == 0;
+
+    println!("{}", if is_ok { "OK" } else { "NG" });
 }
