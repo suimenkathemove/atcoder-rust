79b8b868a5ee5bf3eea7e64e796c1274e5c27a2b
Your Name <you@example.com>
2021-07-28 06:21:54 +0900

    Fix abc114/c

--- abc114/src/bin/c.rs
+++ abc114/src/bin/c.rs
@@ -1,40 +1,27 @@
 use proconio::input;
-use std::collections::HashSet;
 
 fn main() {
     input! {
         N: usize,
     }
 
-    let mut set = HashSet::new();
+    let mut ans = 0;
 
-    fn dfs(
-        current: usize,
-        i: usize,
-        N: usize,
-        set: &mut HashSet<usize>,
-        has_3: bool,
-        has_5: bool,
-        has_7: bool,
-    ) {
-        let current = format!("{}{}", current, i).parse().unwrap();
-
+    fn dfs(current: usize, N: usize, ans: &mut usize, has_3: bool, has_5: bool, has_7: bool) {
         if current > N {
             return;
         }
 
         if has_3 && has_5 && has_7 {
-            set.insert(current);
+            *ans += 1;
         }
 
-        dfs(current, 3, N, set, true, has_5, has_7);
-        dfs(current, 5, N, set, has_3, true, has_7);
-        dfs(current, 7, N, set, has_3, has_5, true);
+        dfs(current * 10 + 3, N, ans, true, has_5, has_7);
+        dfs(current * 10 + 5, N, ans, has_3, true, has_7);
+        dfs(current * 10 + 7, N, ans, has_3, has_5, true);
     }
 
-    dfs(0, 3, N, &mut set, true, false, false);
-    dfs(0, 5, N, &mut set, false, true, false);
-    dfs(0, 7, N, &mut set, false, false, true);
+    dfs(0, N, &mut ans, false, false, false);
 
-    println!("{}", set.len());
+    println!("{}", ans);
 }
