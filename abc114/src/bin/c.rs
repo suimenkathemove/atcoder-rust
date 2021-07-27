84032fb92055b6d5dbef5ac8e92a06e388a732c4
Your Name <you@example.com>
2021-07-28 06:10:00 +0900

    abc114/c

--- abc114/src/bin/c.rs
+++ abc114/src/bin/c.rs
@@ -1,3 +1,40 @@
+use proconio::input;
+use std::collections::HashSet;
+
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+    }
+
+    let mut set = HashSet::new();
+
+    fn dfs(
+        current: usize,
+        i: usize,
+        N: usize,
+        set: &mut HashSet<usize>,
+        has_3: bool,
+        has_5: bool,
+        has_7: bool,
+    ) {
+        let current = format!("{}{}", current, i).parse().unwrap();
+
+        if current > N {
+            return;
+        }
+
+        if has_3 && has_5 && has_7 {
+            set.insert(current);
+        }
+
+        dfs(current, 3, N, set, true, has_5, has_7);
+        dfs(current, 5, N, set, has_3, true, has_7);
+        dfs(current, 7, N, set, has_3, has_5, true);
+    }
+
+    dfs(0, 3, N, &mut set, true, false, false);
+    dfs(0, 5, N, &mut set, false, true, false);
+    dfs(0, 7, N, &mut set, false, false, true);
+
+    println!("{}", set.len());
 }
