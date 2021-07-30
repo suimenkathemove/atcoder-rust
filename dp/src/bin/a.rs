fcae40b2fa886ce62b4e3499348c55f3a1eef228
Your Name <you@example.com>
2021-07-30 10:05:45 +0900

    dp/a

--- dp/src/bin/a.rs
+++ dp/src/bin/a.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        h: [isize; N],
+    }
+
+    let mut cost: Vec<isize> = vec![0; N];
+
+    cost[1] = cost[0] + (h[1] - h[0]).abs();
+
+    for i in 2..N {
+        cost[i] = std::cmp::min(
+            cost[i - 1] + (h[i] - h[i - 1]).abs(),
+            cost[i - 2] + (h[i] - h[i - 2]).abs(),
+        );
+    }
+
+    println!("{}", cost[N - 1]);
 }
