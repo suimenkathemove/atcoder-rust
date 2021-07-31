0f12c5b819831c65610d2fe690a68d06545d2280
Your Name <you@example.com>
2021-08-01 08:59:37 +0900

    Move

--- past201912-open/src/bin/e.rs
+++ past201912-open/src/bin/e.rs
@@ -1,3 +1,63 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        q: usize,
+    }
+
+    let mut g = vec![vec![false; n]; n];
+
+    for _ in 0..q {
+        proconio::input! {
+            t: usize,
+            mut a: usize,
+        }
+        a -= 1;
+
+        match t {
+            1 => {
+                proconio::input! {
+                    mut b: usize,
+                }
+                b -= 1;
+
+                g[a][b] = true;
+            }
+            2 => {
+                for i in 0..n {
+                    if g[i][a] {
+                        g[a][i] = true;
+                    }
+                }
+            }
+            3 => {
+                let mut js = vec![];
+
+                for i in 0..n {
+                    if g[a][i] {
+                        for j in 0..n {
+                            if g[i][j] && j != a {
+                                js.push(j);
+                            }
+                        }
+                    }
+                }
+
+                for j in js {
+                    g[a][j] = true;
+                }
+            }
+            _ => panic!(),
+        }
+    }
+
+    for i in 0..n {
+        for j in 0..n {
+            if g[i][j] {
+                print!("Y");
+            } else {
+                print!("N");
+            }
+        }
+        println!();
+    }
 }
